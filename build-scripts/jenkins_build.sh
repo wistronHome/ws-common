5eae12cde85cebcdd4eb3211d70e769561def3c1
gyjlovelh <18062791691@163.com>
2018-09-04 17:47:53 +0800

    #ws-common 新增 权限类 AuthorityService 全局登陆认证守卫 AuthorityGuardService #

--- build-scripts/jenkins_build.sh
+++ build-scripts/jenkins_build.sh
@@ -1,21 +1,15 @@
 #!/usr/bin/env bash
 
-cd ~
-
 rm -r /usr/local/var/www/html/portal
 rm -r /usr/local/var/www/html/user
 
-cd ./Documents/angular6/ws-common/build-scripts/
-ls
-
+cd ~/Documents/angular6/ws-common/build-scripts/
 source ./build_func.sh
 
-cd ~
-cd ./Documents/angular6/ws-portal/build-scripts/
+cd ~/Documents/angular6/ws-portal/build-scripts/
 source ./build_func.sh
 
-cd ~
-cd ./Documents/angular6/ws-user/build-scripts/
+cd ~/Documents/angular6/ws-user/build-scripts/
 source ./build_func.sh
 
 sudo nginx -s reload
