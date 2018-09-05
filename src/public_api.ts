8ad619d4a980febd747c0be2afd3bdd6eb690d9e
gyjlovelh <18062791691@163.com>
2018-09-05 14:07:16 +0800

    #ws-common 新增 权限类 AuthorityService 全局登陆认证守卫 AuthorityGuardService #

--- src/public_api.ts
+++ src/public_api.ts
@@ -8,9 +8,14 @@ export * from './common/common.module';
 export {LocalStorageService} from './common/service/local-storage/local-storage.service';
 export {AuthorityService} from './common/service/authority/authority.service';
 export {AuthorityGuardService} from './common/service/authority-guard/authority-guard.service';
+export {I18nService} from './common/service/i18n/i18n.service';
+export {CommonI18nService} from './common/service/common-i18n/common-i18n.service';
+export {UrlService} from './common/service/url/url.service';
+export {SystemConfigService} from './common/service/system-config/system-config.service';
 
 // model
 export {User} from './common/model/user.model';
 
 // const
 export {WhitelistRoutes} from './common/const/whitelist-routes';
+export {StorageKey} from './common/const/storage-key';
